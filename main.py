981445884f3cfde646a605c230ee0b2c2902b6d7
Wade Cheng <62270558+amagicmuffin@users.noreply.github.com>
2022-10-02 19:51:01 +0000

    massive update

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ unitTitle = False
 chapterNum = False
 chapterTitle = False
 term = False
-# definition = (under development)
+definition = False
+include_longform = False
+starred_only = False
 
 def updateVocabList():
     with open("concepts.org") as file:
@@ -24,7 +26,8 @@ def updateVocabList():
         for line in file:
             line = line[0:-1]  # strip newline at the end
             
-            # to temp variables above, save current metadata such as Chapter number if the current line denotes it
+            ### to temp variables above, save current metadata such as Chapter number if the current line denotes it
+            # save unit or chapter data
             if len(line) > 5:  # random hardcoded value to avoid error
                 if line[0:3] == "** ":  # line denotes a unit
                     unitLine: str = line[3:]  # strip heading asterisks
@@ -37,15 +40,17 @@ def updateVocabList():
                     currentChapterNum = unitSplit[1]
                     currentChapterName = unitSplit[0]
     
-            # if line is a vocab word, add it to vocabList
-            # TODO ignores emacs bold.
+            # if line is a vocab word, save it to vocabList
+            # TODO doesnt play nice w/ emacs bold.
             if len(line) != 0 and line[0] != "*":  # ignore other headings
+                splitLine = line.split(": ")  # TODO let this be configgable ig
                 vocabList.append({
                     "UnitNum": currentUnitNum,
                     "UnitName": currentUnitName,
                     "ChapterNum": currentChapterNum + " ",  # stop looking at Chapter 12.1 as Chapter 1
                     "ChapterName":currentChapterName,
-                    "Data": line,
+                    "Data": splitLine[0],
+                    "Definition": splitLine[1] if len(splitLine) == 2 else "n/a",
                 })
                 
 # import json  # FOR DEBUG: prettily look at dict
@@ -69,12 +74,18 @@ def updateConfig():
     global chapterNum
     global chapterTitle
     global term
+    global definition
+    global include_longform
+    global starred_only
     include = config["Include"]
     unitNum = include["unitNum"] == "true"
     unitTitle = include["unitTitle"] == "true"
     chapterNum = include["chapterNum"] == "true"
     chapterTitle = include["chapterTitle"] == "true"
     term = include["term"] == "true"
+    definition = include["definition"] == "true"
+    include_longform = include["include_longform"] == "true"
+    starred_only = include["starred_only"] == "true"
 
 
 def getCard():
@@ -94,13 +105,59 @@ def changeIncludeChapters(chapters: str):
 
     updateConfig()
 
+
+def processInput(i):
+    if i == "":
+        return
+
+    # change IncludeChapters config if input includes only digits, commas, or spaces
+    shouldChangeIncludeChapters: bool = True
+    for char in i:
+        if char not in string.digits + ", ":
+            shouldChangeIncludeChapters = False
+            continue
+
+    if shouldChangeIncludeChapters:
+        changeIncludeChapters(i)
+
+    # TODO rewrite changeIncludeChapters() as 
+    # changeIncludeChapters(section: str, key: str, value: str) # use config[section][key]
+            
+
+
 if __name__ == "__main__":
     updateVocabList()
     updateConfig()
 
     while True:
         print("\n"*50)
+
         cardDict = getCard()
+
+        cardFound = False
+        while not cardFound:
+            cardDict = getCard()
+            
+            if starred_only:
+                if cardDict["Data"][0:3] != "[*]":  # this denotes star. make configgable?
+                    cardFound = False
+                    continue
+                
+            if not include_longform:
+                if cardDict["Data"][0] == "+":
+                    cardFound = False
+                    continue
+
+            cardFound = True
+
+        
+        # cardDict = getCard()
+
+        # # if we don't want longform questions (denoted by "+" prefix)
+        # if not include_longform:
+        #     # get a new card until no more "+" prefix
+        #     while cardDict["Data"][0] == "+":
+        #         cardDict = getCard()
 
         # print unit line
         if unitNum:
@@ -121,16 +178,11 @@ if __name__ == "__main__":
         # print term
         if term:
             print(cardDict["Data"])
+            if definition:
+                input()
+                print(cardDict["Definition"])
 
-        # wait forinput
+        # wait for input
         i = input()
-        if i != "":
-            # TODO add changing settings from cmd line
-
-            # change IncludeChapters config if input includes only digits, commas, or spaces
-            for char in i:
-                if char in string.digits + ", ":
-                    changeIncludeChapters(i)
-            
-
-            
+        processInput(i)
+        
